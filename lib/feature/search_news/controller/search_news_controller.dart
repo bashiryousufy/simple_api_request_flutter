b0359fcb65bee007b82fb6f44ad02ee857e2ae0a
bashiryousufy <bashiryousufy@gmail.com>
2021-08-13 12:21:56 +0800

    Added search news functionality

--- lib/feature/search_news/controller/search_news_controller.dart
+++ lib/feature/search_news/controller/search_news_controller.dart
@@ -1,3 +1,46 @@
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:simple_api/core/models/article.dart';
+import 'package:simple_api/core/repository/news_repo.dart';
+import 'package:simple_api/core/repository/news_repo_imp.dart';
 
-class SearchNewsController extends GetxController {}
+class SearchNewsController extends GetxController {
+  late NewsRepo _newsRepo;
+
+  SearchNewsController() {
+    _newsRepo = Get.find<NewsRepoImp>();
+    searchTextController.addListener(() {
+      if (searchTextController.text.length % 3 == 0 &&
+          searchTextController.text.length != 0) {
+        searchNewsHeadline();
+      }
+    });
+  }
+
+  final searchTextController = TextEditingController();
+
+  RxBool isLoading = false.obs;
+
+  RxList<Article> articles = RxList();
+
+  searchNewsHeadline() async {
+    showLoading();
+    final result = await _newsRepo.getSearchNews(searchTextController.text);
+
+    hideLoading();
+
+    if (result != null) {
+      articles = result.obs;
+    } else {
+      print("No Data Recieved!");
+    }
+  }
+
+  showLoading() {
+    isLoading.toggle();
+  }
+
+  hideLoading() {
+    isLoading.toggle();
+  }
+}
